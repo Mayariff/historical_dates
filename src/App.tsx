ffdfa871fdd5c0adac02e82ce7c140d323d774f8
Maya <79027972026@yandex.ru>
2022-09-16 08:19:23 +0500

    change mobile layout for slideBar

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,39 @@ function App() {
     const boxRef = useRef<HTMLInputElement | null>(null);
     const [clickCoords, setClickCoords] = useState({pageX: 0, pageY: 0});
 
+    const [screenWidth, setScreenWidth] = useState<number>(0)
+    const isDesctopVertion = screenWidth > 768
+
     useEffect(() => {
-        const anim = gsap.fromTo(boxRef.current, {
-            width: '52px',
-            height: '52px',
-            borderRadius: '50%',
-            position: 'absolute',
-            zIndex: '1',
-            left: clickCoords.pageX,
-            top: clickCoords.pageY,
-            border: '3px solid #42567A',
-            opacity: '1',
-            transition: '0.5s cubic-bezier (.75,-1.27,.3,2.33) transform, .2s cubic-bezier (.75,-1.27,.3,2.33) opacity',
-        }, {opacity: '0', delay: 0.1})
-        return () => {
-            anim.kill()
+        function findWidth() {
+            setScreenWidth(window.screen.width)
         }
-    }, [clickCoords])
+
+        findWidth()
+        window.addEventListener('resize', findWidth);
+        return () => window.removeEventListener('resize', findWidth)
+    }, [window.screen.width])
+
+    useEffect(() => {
+        if (isDesctopVertion) {
+            const anim = gsap.fromTo(boxRef.current, {
+                width: '52px',
+                height: '52px',
+                borderRadius: '50%',
+                position: 'absolute',
+                zIndex: '1',
+                left: clickCoords.pageX,
+                top: clickCoords.pageY,
+                border: '3px solid #42567A',
+                opacity: '1',
+                scale: 0.6,
+                transition: '0.5s cubic-bezier (.75,-1.27,.3,2.33) transform, .2s cubic-bezier (.75,-1.27,.3,2.33) opacity',
+            }, {opacity: '0', delay: 0.1, scale: 1})
+            return () => {
+                anim.kill()
+            }
+        }
+    }, [clickCoords, isDesctopVertion])
 
 
     const clickHandler: MouseEventHandler<HTMLDivElement> = (e) => {
@@ -35,7 +51,7 @@ function App() {
     return (
         <div onClick={clickHandler}>
             <div ref={boxRef}> </div>
-            <ItemsBlock data={data} title={'Истoрические даты'}/>
+            <ItemsBlock data={data} title={'Истoрические даты'} isDesctopVertion={isDesctopVertion}/>
         </div>
 
     );
--- src/Blocks/ItemsBlock/ItemsBlock.tsx
+++ src/Blocks/ItemsBlock/ItemsBlock.tsx
@@ -10,24 +10,14 @@ import SliderBar from '../../Components/SliderBar/SliderBar';
 type propsType = {
     data: periodsInfoType[]
     title: string
+    isDesctopVertion: boolean
 }
 
-const ItemsBlock = ({data, title}: propsType) => {
+const ItemsBlock = ({data, title, isDesctopVertion}: propsType) => {
 
     const [currentPeriod, setCurrentPeriod] = useState<periodsInfoType>(data[0])
     const [currentPage, setCurrentPage] = useState<number>(1)
-    const [screenWidth, setScreenWidth] = useState<number>(0)
-    const isDesctopVertion = screenWidth > 768
 
-    useEffect(() => {
-        function findWidth() {
-            setScreenWidth(window.screen.width)
-        }
-
-        findWidth()
-        window.addEventListener('resize', findWidth);
-        return () => window.removeEventListener('resize', findWidth)
-    }, [window.screen.width])
 
     useEffect(() => {
             setCurrentPeriod(data[currentPage - 1])
